d68daa618a3b56ab77b11c396e195f1039acdba9
Julius <julius.demartini@gmail.com>
2024-06-30 21:47:32 -0300

    fix: required cors

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const { connection } = require('./dbconnect/dbconnect')
 const port = 3000
 
 const webRoutes = require('./routes/routes')
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  )
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  next()
-});
+app.use(express.json())
 
-app.use('/api', webRoutes);
+app.use(cors({
+  origin: 'https://capybaragames-vue3.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  credentials: true
+}))
+
+app.options('*', cors({
+  origin: 'https://capybaragames-vue3.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  credentials: true
+}))
+
+app.use('/api', webRoutes)
 
 app.get('/', async (req, res) => {
   try {
@@ -25,8 +32,8 @@ app.get('/', async (req, res) => {
     console.error('Error en la consulta:', err.stack)
     res.status(500).send('Error en la base de datos')
   }
-});
+})
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`)
-});
+})
