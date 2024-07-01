ef6505d4a43c3c15d84538b4376d603f3037d76d
Maty910 <matychacong@gmail.com>
2024-07-01 17:10:39 -0300

    feat: multer

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const game = require('../controllers/gameController')
+const game = require('../controllers/gamecontroller')
 const config = require('../controllers/configController')
 const login = require('../controllers/loginController')
+const multer = require('multer')
 const auth = require('../config/auth')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, `src/assets/img/`)
+    },
+    filename: (req, file, cb) =>{
+        console.log(file);
+        cb(null, Date.now() + "_" + file.originalname)
+    }
+})
+
+const uploadFile = multer({storage})
 
 router.post('/login', login.login)
 
 router.get('/games', game.getGames)
 
 router.get('/game/:id', game.getGame)
-router.post('/game', auth, game.createGame)
+router.post('/game', auth, uploadFile.single('image'), game.createGame)
 router.put('/game/:id', auth, game.editGame)
 router.delete('/game/:id', auth, game.deleteGame)
 
