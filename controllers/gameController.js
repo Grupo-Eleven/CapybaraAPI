2da33becfb700626fe8774d3c8a7e2c694afc8ad
Julius <154571890+Julius-dmrt@users.noreply.github.com>
2024-07-01 18:32:56 +0000

    fix: update order error

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,81 +1,94 @@
-const { connection } = require('../dbconnect/dbconnect')
+const { connection } = require("../dbconnect/dbconnect");
 
 exports.getGames = async (req, res) => {
     try {
-        const [results, fields] = await connection.query('SELECT * FROM videojuegos')
-        res.json(results)
+        const [results, fields] = await connection.query(
+            "SELECT * FROM videojuegos",
+        );
+        res.json(results);
     } catch (err) {
-        console.error('Error en la consulta:', err.stack)
-        res.status(500).send('Error en la base de datos')
+        console.error("Error en la consulta:", err.stack);
+        res.status(500).send("Error en la base de datos");
     }
-}
+};
 
 exports.getGame = async (req, res) => {
     try {
-        const { id } = req.params
+        const { id } = req.params;
         if (!id) {
-            return res.status(400).send('ID is required')
+            return res.status(400).send("ID is required");
         }
-        const query = 'SELECT * FROM videojuegos WHERE id = ?'
-        const [results, fields] = await connection.query(query, [id])
+        const query = "SELECT * FROM videojuegos WHERE id = ?";
+        const [results, fields] = await connection.query(query, [id]);
         if (results.length === 0) {
-            return res.status(404).send('Game not found')
+            return res.status(404).send("Game not found");
         }
-        res.json(results[0])
+        res.json(results[0]);
     } catch (err) {
-        console.error('Error en la consulta:', err.stack)
-        res.status(500).send('Error en la base de datos')
+        console.error("Error en la consulta:", err.stack);
+        res.status(500).send("Error en la base de datos");
     }
-}
+};
 
 exports.createGame = async (req, res) => {
-    const { nombre, url, about, plataforma, categoria, home, img } = req.body
-    const query = 'INSERT INTO videojuegos (nombre, url, about, plataforma, categoria, home, img) VALUES (?, ?, ?, ?, ?, ?, ?)'
-    const values = [nombre, url, about, plataforma, categoria, home, img]
-    console.log(values)
+    const { nombre, url, about, plataforma, categoria, home, img } = req.body;
+    const query =
+        "INSERT INTO videojuegos (nombre, url, about, plataforma, categoria, home, img) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const values = [nombre, url, about, plataforma, categoria, home, img];
+    console.log(values);
 
     try {
-        const [results, fields] = await connection.query(query, values)
-        res.status(201).json({ id: results.insertId, nombre, url, about, plataforma, categoria, home, img })
+        const [results, fields] = await connection.query(query, values);
+        res.status(201).json({
+            id: results.insertId,
+            nombre,
+            url,
+            about,
+            plataforma,
+            categoria,
+            home,
+            img,
+        });
     } catch (err) {
-        console.error('Error en la consulta:', err.stack)
-        res.status(500).send('Error en la base de datos')
+        console.error("Error en la consulta:", err.stack);
+        res.status(500).send("Error en la base de datos");
     }
-}
+};
 
 exports.editGame = async (req, res) => {
-    const { id } = req.params
-    const { nombre, url, about, plataforma, categoria, home, img } = req.body
-    const query = 'UPDATE videojuegos SET nombre = ?, url = ?, about = ?, categoria = ?, plataforma = ?, home = ?, img = ? WHERE id = ?'
-    const values = [nombre, url, about, plataforma, categoria, home, img, id]
-
+    const { id } = req.params;
+    const { nombre, url, about, plataforma, categoria, home, img } = req.body;
+    const query =
+        "UPDATE videojuegos SET nombre = ?, url = ?, about = ?, plataforma = ?, categoria = ?, home = ?, img = ? WHERE id = ?";
+    const values = [nombre, url, about, plataforma, categoria, home, img, id];
+    console.log(values);
     try {
-        const [results, fields] = await connection.query(query, values)
+        const [results, fields] = await connection.query(query, values);
         if (results.affectedRows === 0) {
-            res.status(404).send('Videojuego no encontrado')
+            res.status(404).send("Videojuego no encontrado");
             return;
         }
-        res.json({ id, nombre, url, about, plataforma, categoria, home, img })
+        res.json({ id, nombre, url, about, plataforma, categoria, home, img });
     } catch (err) {
-        console.error('Error en la consulta:', err.stack)
-        res.status(500).send('Error en la base de datos')
+        console.error("Error en la consulta:", err.stack);
+        res.status(500).send("Error en la base de datos");
     }
-}
+};
 
 exports.deleteGame = async (req, res) => {
-    const { id } = req.params
-    const query = 'DELETE FROM videojuegos WHERE id = ?'
-    const values = [id]
+    const { id } = req.params;
+    const query = "DELETE FROM videojuegos WHERE id = ?";
+    const values = [id];
 
     try {
-        const [results, fields] = await connection.query(query, values)
+        const [results, fields] = await connection.query(query, values);
         if (results.affectedRows === 0) {
-            res.status(404).send('Videojuego no encontrado')
-            return
+            res.status(404).send("Videojuego no encontrado");
+            return;
         }
-        res.json({ message: 'Videojuego eliminado' })
+        res.json({ message: "Videojuego eliminado" });
     } catch (err) {
-        console.error('Error en la consulta:', err.stack)
-        res.status(500).send('Error en la base de datos')
+        console.error("Error en la consulta:", err.stack);
+        res.status(500).send("Error en la base de datos");
     }
-}
+};
